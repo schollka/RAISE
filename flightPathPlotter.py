200f3ab3bc2ec204023242f37c8f1ce1e8af0984
schollka <web.scholl@t-online.de>
2025-05-23 20:54:43 +0200

    changes plotter

--- flightPathPlotter.py
+++ flightPathPlotter.py
@@ -12,6 +12,7 @@ class FlightPlotter:
         self.aircraftColors = {}
         self.colorCycle = itertools.cycle(plt.cm.get_cmap('tab10').colors)
         self.lines = {}
+        self.verbose = 0
 
         self.ax.set_xlabel("Longitude")
         self.ax.set_ylabel("Latitude")
@@ -24,19 +25,18 @@ class FlightPlotter:
         self.ax.set_ylabel("Latitude")
         self.ax.set_title("Live Flight Tracks")
         self.ax.grid(True)
-        self.ax.set_aspect('equal', adjustable='datalim')  # ⬅️ This line sets equal scaling
-
+        self.ax.set_aspect('equal', adjustable='datalim')
 
         # Flughafenmarker zeichnen
         self.ax.plot(self.airportLon, self.airportLat, marker='x', color='black', markersize=12, mew=3, label='Airport')
 
-        # Kreis um den Flughafen (nur optisch – nicht exakt metrisch genau auf Kugeloberfläche)
         # Für kleine Radien ist eine grobe Umrechnung ausreichend
         kmPerDegreeLat = 111  # Näherung
         kmPerDegreeLon = 111 * abs(math.cos(math.radians(self.airportLat)))
         radiusDegLat = self.airportRadius_km / kmPerDegreeLat
         radiusDegLon = self.airportRadius_km / kmPerDegreeLon
 
+        # Kreis zeichnen
         circle = Circle(
             (self.airportLon, self.airportLat),
             radius=radiusDegLon,
@@ -47,6 +47,13 @@ class FlightPlotter:
             label=f'{self.airportRadius_km*1000:.0f} m Radius'
         )
         self.ax.add_patch(circle)
+
+        # Plotbereich auf ±2,5 km beschränken
+        halfExtentKm = 2.5
+        extentDegLat = halfExtentKm / kmPerDegreeLat
+        extentDegLon = halfExtentKm / kmPerDegreeLon
+        self.ax.set_xlim(self.airportLon - extentDegLon, self.airportLon + extentDegLon)
+        self.ax.set_ylim(self.airportLat - extentDegLat, self.airportLat + extentDegLat)
 
         for aircraftId, trackInfo in aircraftTracks.items():
             track = trackInfo["track"]
@@ -65,8 +72,12 @@ class FlightPlotter:
             # Plot last known position as black rectangle
             lastLat = lats[-1]
             lastLon = lons[-1]
-            self.ax.plot(lastLon, lastLat, marker='s', color='black', markersize=5)
-
+            if trackInfo['state'] == "airborne":
+                self.ax.plot(lastLon, lastLat, marker='s', color='black', markersize=5)
+            elif trackInfo['state'] == "onGround":
+                self.ax.plot(lastLon, lastLat, marker='^', color='blue', markersize=3)
+            else:
+                self.ax.plot(lastLon, lastLat, marker='o', color='green', markersize=3)
 
         self.ax.legend(loc='upper right')
 
